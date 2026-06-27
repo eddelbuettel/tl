cc300e60382bd5b5cf2de529dfc8e9e83f63aa06
Dirk Eddelbuettel <edd@debian.org>
2026-06-27 06:43:02 -0500

    Condition header on C++20, fallback and message if not

--- inst/include/tl.h
+++ inst/include/tl.h
@@ -3,6 +3,9 @@
 
 namespace tl {
 
+// In order to survive on CRAN, we have to cope with non-C++20 compilation
+#if  __cplusplus >= 202002L
+
     template<class... Args>
     void trace(spdlite::format_string_t<Args...> s, Args&&... args) {
         rspdlite::log_trace(s, std::forward<Args>(args)...);
@@ -38,4 +41,24 @@ namespace tl {
 
     inline void        set_name(const std::string& s) { rspdlite::set_name(s);       }
     inline std::string get_name()                     { return rspdlite::get_name(); }
+
+#else
+
+    // C++17 and below fallback to permit compilation
+    #if defined(__GNUC__) || defined(__clang__)
+        #pragma message("You need to enable C++20 to for tl / rspdlite / spdlite logging.")
+    #endif
+
+    inline void trace(std::string, ...) {}
+    inline void debug(std::string, ...) {}
+    inline void info(std::string, ...) {}
+    inline void warn(std::string, ...) {}
+    inline void error(std::string, ...) {}
+    inline void critical(std::string, ...) {}
+    inline void set_level(std::string, ...) {}
+    inline std::string get_level() { return std::string(); }
+    inline void set_name(std::string) {}
+    inline std::string get_name() { return std::string(); }
+
+#endif
 }
